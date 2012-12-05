3a54c7b20db380346d58a10d3101f12a6c84ed42
Jason Morganson <jason@morganson.me>
2012-12-05 04:55:24 +0000

    Updates for basic server file

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
+process.env.NODE_ENV = 'development';
+
 var express = require( 'express' ),
     consolidate = require( 'consolidate' ),
     app = express();
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 
 // Assign the jade engine to .html files
 app.engine( 'html', consolidate.jade );
 
-// Set .html as the default extension 
+// Set .html as the default extension
 app.set( 'view engine', 'html' );
 app.set( 'views', __dirname + '/views' );
 
@@ -15,6 +17,13 @@ app.use( express.static( __dirname ) );
 app.use( express.static( __dirname + '/js' ) );
 app.use( express.static( __dirname + '/css' ) );
 app.use( express.static( __dirname + '/font' ) );
+app.use( express.static( __dirname + '/profiles' ) );
+app.use( express.static( __dirname + '/.well-known' ) );
+
+app.get( '/find.me', function( req, res ) {
+
+    res.render('find', { bodyClass: 'terminal' } );
+});
 
 app.configure( 'development', function() {
   app.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) );
