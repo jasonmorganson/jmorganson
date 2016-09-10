fdfa58a5534c7a724de062abb2e73b7bae946556
Jason Morganson <jason@morganson.me>
2016-09-10 09:26:53 -0400

    Let env configure port

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const Koa = require('koa')
 const serve = require('koa-static')
 const server = new Koa()
 
+let port = process.env.PORT || 80
+
 server.use(serve('./build'))
 server.use(serve('./files'))
 
-server.listen(80)
+server.listen(port)
 
